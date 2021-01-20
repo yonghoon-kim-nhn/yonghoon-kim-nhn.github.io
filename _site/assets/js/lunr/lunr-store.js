555dc14e8295a9a04d098a171b1edc455fac0b09
yonghoon-kim-nhn <yonghoon.kim@nhn.com>
2021-01-20 21:08:48 +0900

    test 1.0

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "서시 - 윤동주",
+        "excerpt":" ","categories": ["윤동주"],
+        "tags": ["서시","윤동주","시"],
+        "url": "https://yonghoon-kim-nhn.github.io/%EC%9C%A4%EB%8F%99%EC%A3%BC/si-susi/",
+        "teaser": "https://yonghoon-kim-nhn.github.io/assets/images/logo.png"
+      }]
